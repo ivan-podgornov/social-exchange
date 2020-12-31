2e57b02ded75553394300cf2ebaac2d884e0fae5
ivan-podgornov <ivan.podgornov@live.com>
2020-12-31 19:08:33 +0300

    refactor: правильно использую fp-ts в сервисе авторизации

--- packages/core/modules/networks/networks.service.ts
+++ packages/core/modules/networks/networks.service.ts
@@ -1,9 +1,12 @@
+import { Option, none, some } from 'fp-ts/Option';
+import { Task } from 'fp-ts/Task';
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { left, right } from 'fp-ts/Either';
 import { Network } from './network.entity';
 import { Incognito, NetworkType, User } from '@social-exchange/types';
+import { UserEntity } from 'modules/users/user.entity';
 
 @Injectable()
 export class NetworksService {
@@ -20,12 +23,14 @@ export class NetworksService {
         return userNetwork;
     }
 
-    /** Ищет владельца указанной социальной сети */
-    async findOwner(network: Incognito) {
-        const userNetwork = await this.networks
-            .findOne({ uid: network.uid }, { relations: ['owner'] });
-        if (!userNetwork) return left(null);
-        return right(userNetwork.owner);
+    /** Ищет владельца указанного профиля социальной сети */
+    findOwner(network: Incognito): Task<Option<User>> {
+        return async () => {
+            const uid = network.uid;
+            const options = { relations: ['owner'] };
+            const profile = await this.networks.findOne({ uid }, options);
+            return profile ? some(profile.owner) : none;
+        };
     }
 
     /** Ищет социальную сеть по id пользователя в этой сети */
